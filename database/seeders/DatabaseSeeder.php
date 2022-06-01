f3d585d98f5a0ae9a2255481c8d69560fb321f52
Luis Velazquez <luis.velazquez@ignitionds.com>
2022-05-31 21:36:31 -0500

    Instructions DB init - OK

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         
+        /**
+         * Generate all users test
+         */
         User::factory()->create([
              'name' => 'Luis Dirceu Velázquez Fuentes',
              'email' => 'krammstein@gmail.com',
